db907c6bba48e7085b45b6eff77b5129a4b8ebc1
Poh Soon Chang <scpoh.phd@gmail.com>
2024-01-08 22:47:06 +0800

    Delete codes/phi-1_5 directory

--- codes/phi-1_5/finetune_classifier.py
+++
@@ -1,7 +0,0 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
-model_checkpoint = '/media/cybertron/fa54fcb6-b5e1-492e-978a-6389519c168a/sparsegpt/output/phi-1.5'
-tokenizer = AutoTokenizer.from_pretrained(model_checkpoint, use_fast=True)
-model = AutoModelForCausalLM.from_pretrained(model_checkpoint) #, device_map='auto') #,trust_remote_code=True)
-
-for k,v in model.named_parameters():
-    print(k,v.shape)
